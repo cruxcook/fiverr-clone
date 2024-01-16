329b5fb84d773384699f05e13d6f79ac0d2d29b8
Crux Cook <cruxcook@tutanota.com>
2024-01-15 21:25:53 -0800

    feat(navbar): add extra Navbar when scrolling
    Issue #1

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <span className="text">fiverr</span>
@@ -18,6 +32,23 @@ const Navbar = () => {
           <button>Join</button>
         </div>
       </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Graphics & Design</span>
+            <span>Programming & Tech</span>
+            <span>Digital Marketing</span>
+            <span>Video & Animation</span>
+            <span>Writing & Translation</span>
+            <span>Music & Audio</span>
+            <span>Business</span>
+            <span>Data</span>
+            <span>Photography</span>
+            <span>AI Services</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
